7e6c48e4b72bee431b62a56263a8206ca9fb4fe2
jerryman23 <130792742+jerryman23@users.noreply.github.com>
2023-05-02 21:23:20 +0000

    Tue, May 2, 2023, 9:23 PM +00:00

--- mario.c
+++ mario.c
@@ -4,6 +4,10 @@
 int main(void)
 {
     int n = get_int("Size: ");
+    if (n < 1)
+    {
+        n = get_int("Size: ");
+    }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
