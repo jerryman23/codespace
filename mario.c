de004bbe79a633e58f176c5ea40007c257bda814
jerryman23 <130792742+jerryman23@users.noreply.github.com>
2023-05-02 21:10:03 +0000

    Tue, May 2, 2023, 9:10 PM +00:00

--- mario.c
+++ mario.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    const int n = 3;
+    int n = get_int("Size: ");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
