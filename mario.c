a0567976312754d26c2ace3b44ace21975aaaa9a
jerryman23 <130792742+jerryman23@users.noreply.github.com>
2023-05-02 20:54:57 +0000

    Tue, May 2, 2023, 8:54 PM +00:00

--- mario.c
+++ mario.c
@@ -4,9 +4,9 @@ int main(void)
 {
     for (int i = 0; i < 3; i++)
     {
-        for (int i = 0; i < 3; i++)
+        for (int i2 = 0; i2 < 3; i2++)
         {
-            
+            printf("#");
         }
     }
 }
