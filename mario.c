31331e32d5ca4f7ab6e6d35ac53553b0460c1399
jerryman23 <130792742+jerryman23@users.noreply.github.com>
2023-05-04 19:12:00 +0000

    Thu, May 4, 2023, 7:12 PM +00:00

--- mario.c
+++ mario.c
@@ -3,13 +3,15 @@
 
 int main(void)
 {
-    int n = get_int("Size: ");
+    //get size of grid
+    int n;
     do
     {
         n = get_int("Size: ");
     }
     while (n < 1);
-    
+
+    //print grid blocks
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
