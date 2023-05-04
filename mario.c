338a34cf7c4b864383010f8950f5941d3deb2e49
jerryman23 <130792742+jerryman23@users.noreply.github.com>
2023-05-04 19:18:57 +0000

    Thu, May 4, 2023, 7:18 PM +00:00

--- mario.c
+++ mario.c
@@ -7,14 +7,7 @@ int main(void)
     int n = get_size();
 
     //print grid blocks
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
+    print_grid(n);
 }
 
 int get_size(void)
@@ -22,8 +15,26 @@ int get_size(void)
     int n;
     do
     {
-        n = get_int("Size: ")
+        n = get_int("Size: ");
     }
     while (n < 1);
     return n;
+}
+
+
+
+
+
+
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
